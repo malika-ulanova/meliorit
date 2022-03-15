87808ec7c67fc6a3078da40a3f99e6a82240ad64
malika-ulanova <mulanova092@gmail.com>
2022-03-15 04:09:17 -0700

    intial

--- js/main.js
+++ js/main.js
@@ -1,25 +1,47 @@
 $('.popular_slide').slick({
     slidesToShow: 4,
-    responsive:[{
-        breakpoint:1024,
-        settings:{
-            slidesToShow: 3,
+    responsive:
+        [
+            {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow:3,
+        }
         },
 
-        breakpoint:937,
-        settings:{
+            {
+        breakpoint: 937,
+        settings: {
             slidesToShow: 2,
-        },
 
-        breakpoint:635,
-        settings:{
+        }
+        },
+            {
+        breakpoint: 635,
+        settings: {
             slidesToShow: 1,
         }
-
     }
-]
+        ]
 })
 
 $('.subscriptions_slide').slick({
     slidesToShow: 3,
+    responsive:
+        [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow:2,
+                }
+            },
+
+
+            {
+                breakpoint: 635,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
 })
